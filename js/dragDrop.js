8aa6979d88ecc9935f04ab1b94094b570642f770
Jesús Barajas <barajin21@gmail.com>
2018-04-25 22:04:14 -0600

    ya entendí como llenar el arreglo, pero no se llena

--- js/dragDrop.js
+++ js/dragDrop.js
@@ -1,58 +1,82 @@
 		var tablero = new Array(9);
+    var fotoid;
+
+
+    function asignarjugador(fotoid){
+      var tipojugador;
+      if(fotoid==="equis"){
+        vartipojugador=0;
+        jugadorX();
+      }else if(fotoid==="circulo")
+      vartipojugador=0;
+      jugadorO();
+    }
+
 
 
 	function drop(ev){
+
 			ev.preventDefault();
 			var data=ev.dataTransfer.getData("text");
 			ev.target.appendChild(document.getElementById(data))
 			var elemento=ev.target.id;	
 			console.log(elemento);
-			tablero[elemento]=1;	
+      var fotoid=asignarjugador();
+      console.log(fotoid);
+      if(fotoid=="equis"){
+        tablero[elemento]=0;
+      }else if(fotoid=="circulo")
+      tablero[elemento]=1;
+     
+				
 		}
 		function allowDrop(ev){
 			ev.preventDefault();
 		}
 		function drag(ev){
 			ev.dataTransfer.setData("text",ev.target.id);
+      var fotoid=ev.target.id; 
+      asignarjugador(fotoid);
+      console.log(fotoid); 
 		}
 
 function jugadorX(){
         //para el jugador X
-        if(opciones[1]==1 && opciones[2]==1 && opciones[3]==1){//primer linea horizontal
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[4]==1 && opciones[5]==1 && opciones[6]==1){//segunda linea horizontal
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[7]==1 && opciones[8]==1 && opciones[9]==1){//tercera linea horizontal
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[1]==1 && opciones[5]==1 &&opciones[9]==1){//primera linea diagonal
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[3]==1 && opciones[5]==1 &&opciones[7]==1){//segunda linea diagonal
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[1]==1 && opciones[4]==1 &&opciones[7]==1){//primer linea vertical izquierda
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[2]==1 && opciones[5]==1 &&opciones[8]==1){//linea de enmedio vertical
-          alert("Felicidades Jugador " +turno+ " Ganaste");
-        }else if(opciones[3]==1 && opciones[6]==1 &&opciones[9]==1){//tercer linea vertical derecha
-          alert("Felicidades Jugador " +turno+ " Ganaste");
+        if(tablero[1]==1 && tablero[2]==1 && tablero[3]==1){//primer linea horizontal
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[4]==1 && tablero[5]==1 && tablero[6]==1){//segunda linea horizontal
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[7]==1 && tablero[8]==1 && tablero[9]==1){//tercera linea horizontal
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[1]==1 && tablero[5]==1 &&tablero[9]==1){//primera linea diagonal
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[3]==1 && tablero[5]==1 &&tablero[7]==1){//segunda linea diagonal
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[1]==1 && tablero[4]==1 &&tablero[7]==1){//primer linea vertical izquierda
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[2]==1 && tablero[5]==1 &&tablero[8]==1){//linea de enmedio vertical
+          alert("Felicidades Jugador X Ganaste");
+        }else if(tablero[3]==1 && tablero[6]==1 &&tablero[9]==1){//tercer linea vertical derecha
+          alert("Felicidades Jugador X Ganaste");
         }
                 }
                 // para el jugador 2
                function jugadorO(){
-        if(opciones[1]==0 && opciones[2]==0 && opciones[3]==0){//primer linea horizontal
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[4]==0 && opciones[5]==0 && opciones[6]==0){//segunda linea horizontal
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[7]==0 && opciones[8]==0 && opciones[9]==0){//tercera linea horizontal
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[1]==0 && opciones[5]==0 &&opciones[9]==0){//primera linea diagonal
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[3]==0 && opciones[5]==0 &&opciones[7]==0){//segunda linea diagonal
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[1]==0 && opciones[4]==0 &&opciones[7]==0){//primer linea vertical izquierda
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[2]==0 && opciones[5]==0 &&opciones[8]==0){//linea de enmedio vertical
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
-        }else if(opciones[3]==0 && opciones[6]==0 &&opciones[9]==0){//tercer linea vertical derecha
-          alert("Felicidades Jugador " +turno2+ " Ganaste");
+        if(tablero[1]==0 && tablero[2]==0 && tablero[3]==0){//primer linea horizontal
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[4]==0 && tablero[5]==0 && tablero[6]==0){//segunda linea horizontal
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[7]==0 && tablero[8]==0 && tablero[9]==0){//tercera linea horizontal
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[1]==0 && tablero[5]==0 &&tablero[9]==0){//primera linea diagonal
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[3]==0 && tablero[5]==0 &&tablero[7]==0){//segunda linea diagonal
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[1]==0 && tablero[4]==0 &&tablero[7]==0){//primer linea vertical izquierda
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[2]==0 && tablero[5]==0 &&tablero[8]==0){//linea de enmedio vertical
+          alert("Felicidades JugadorO Ganaste");
+        }else if(tablero[3]==0 && tablero[6]==0 &&tablero[9]==0){//tercer linea vertical derecha
+          alert("Felicidades JugadorO Ganaste");
         }
                }
